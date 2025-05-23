8e522440fcd7809267036b1fc9bd35a47b0c88a1
charain <charain_li@outlook.com>
2025-05-24 00:24:12 +0800

    add ebreak exit

--- riscv/insns/ebreak.h
+++ riscv/insns/ebreak.h
@@ -4,7 +4,11 @@ if (!STATE.debug_mode && (
         (!STATE.v && STATE.prv == PRV_U && STATE.dcsr->ebreaku) ||
         (STATE.v && STATE.prv == PRV_S && STATE.dcsr->ebreakvs) ||
         (STATE.v && STATE.prv == PRV_U && STATE.dcsr->ebreakvu))) {
+      printf("Spike exited normally due to ebreak instruct\n");
+      exit(0);
 	throw trap_debug_mode();
 } else {
+      printf("Spike exited normally due to ebreak instruct\n");
+      exit(0);
 	throw trap_breakpoint(STATE.v, pc);
 }
